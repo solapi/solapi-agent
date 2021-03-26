ffd8377f5561dd811f8f85da7030eb49d52b49bc
wileyday <wiley@nurigo.net>
2021-03-26 14:55:29 +0900

    예약 발송 샘플 쿼리 추가

--- samples.sql
+++ samples.sql
@@ -71,3 +71,13 @@ INSERT INTO msg(payload) VALUES(json_object(
     ))
   )
 ));
+
+-- 발송 예약 (2021년 3월 26일 12시 20분에 발송 예약)
+INSERT INTO msg(scheduledAt, payload) VALUES(
+  '2021-03-26 12:20:00',
+  json_object(
+    'to', '01000000001',
+    'from', '020000001',
+    'text', '테스트 메시지'
+  )
+);
